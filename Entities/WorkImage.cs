1d91fcab45b324b93777d432e941bf145748b0b4
Husnain Abbas <hassnain95@gmail.com>
2020-03-18 10:47:42 +0500

    WorkDoneImage-Validation

--- Entities/WorkImage.cs
+++ Entities/WorkImage.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApi.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Entities
 {
     public class WorkImage
     {
         public int Id { get; set; }
+        [Required]
         public string FilePath { get; set; }
         public virtual WorkDone WorkDone { get; set; }
         public int WorkDoneId { get; set; }
