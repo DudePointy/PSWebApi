e3e001152a3ef4a422f5d3d202840ba2e3727c61
Husnain Abbas <hassnain95@gmail.com>
2020-03-18 10:17:18 +0500

    OrderMessage-Validation

--- Entities/OrderMessage.cs
+++ Entities/OrderMessage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.Entities
 {
@@ -11,6 +12,7 @@ namespace WebApi.Entities
         public virtual User Sender { get; set; }
         public int SenderId { get; set; }
 
+        [Required]
         public string Text { get; set; }
         public DateTime SentOn { get; set; }
     }
