8cda544bdcb66774c9fb64e768882af1259884ef
Husnain Abbas <hassnain95@gmail.com>
2020-03-19 09:44:20 +0500

    register implementation with password encryption and dycrption

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,8 +3,10 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using WebApi.Entities;
 using WebApi.Helpers;
@@ -25,6 +27,16 @@ namespace WebApi.Services
         private readonly ApplicationDbContext db;
 
         private readonly AppSettings _appSettings;
+
+        public User Register(User user)
+        {
+            user.Password = Encrypt(user.Password);
+            user.RoleId = 1;
+            db.ApplicationUsers.Add(user);
+            db.SaveChanges();
+            return Authenticate(user.Username, Decrypt(user.Password));
+
+        }
 
         public UserService(IOptions<AppSettings> appSettings, ApplicationDbContext context)
         {
@@ -69,6 +81,40 @@ namespace WebApi.Services
         {
             var user = db.ApplicationUsers.FirstOrDefault(x => x.Id == id);
             return user.WithoutPassword();
+        }
+
+        public string Encrypt(string str)
+        {
+            string EncrptKey = "2013;[pnuLIT)WebCodeExpert";
+            byte[] byKey = { };
+            byte[] IV = { 18, 52, 86, 120, 144, 171, 205, 239 };
+            byKey = System.Text.Encoding.UTF8.GetBytes(EncrptKey.Substring(0, 8));
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.UTF8.GetBytes(str);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(byKey, IV), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public string Decrypt(string str)
+        {
+            str = str.Replace(" ", "+");
+            string DecryptKey = "2013;[pnuLIT)WebCodeExpert";
+            byte[] byKey = { };
+            byte[] IV = { 18, 52, 86, 120, 144, 171, 205, 239 };
+            byte[] inputByteArray = new byte[str.Length];
+
+            byKey = System.Text.Encoding.UTF8.GetBytes(DecryptKey.Substring(0, 8));
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            inputByteArray = Convert.FromBase64String(str.Replace(" ", "+"));
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(byKey, IV), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
+            return encoding.GetString(ms.ToArray());
         }
     }
 }
