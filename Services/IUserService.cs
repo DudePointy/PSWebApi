975ae1953e4fa24a8dcd1eb70df5915361229f71
Husnain Abbas <hassnain95@gmail.com>
2020-03-19 09:43:41 +0500

    Register method prototype

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -8,5 +8,6 @@ namespace WebApi.Services
         User Authenticate(string username, string password);
         IEnumerable<User> GetAll();
         User GetById(int id);
+        User Register(User user);
     }
 }
